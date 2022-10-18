d400f5696f7f250583709ee3f08d02164f062064
Tommy Ledford <tledford58@gmail.com>
2022-10-18 15:58:38 -0700

    Initial working version

---
+++ src/GarageAccessory.ts
@@ -0,0 +1,278 @@
+import {
+  Service,
+  PlatformAccessory,
+  CharacteristicValue,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  Characteristic,
+  HAPStatus,
+} from 'homebridge';
+import { URLSearchParams } from 'url';
+import AbortController from 'abort-controller';
+
+import { ParticleHomebridgePlatform } from './platform';
+
+const fetch = require('node-fetch');
+const eventSource = require('eventsource');
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class GarageOpenerPlatformAccessory {
+  private garage_service: Service;
+
+  private CurrentDoorState: typeof Characteristic.CurrentDoorState;
+  private TargetDoorState: typeof Characteristic.TargetDoorState;
+
+  private Service: typeof Service;
+  private Characteristic: typeof Characteristic;
+
+  private accessToken: string;
+  private deviceId: string;
+  private url: string = 'https://api.particle.io/v1/';
+  private doorOpensInSeconds: number;
+  private eventName: string;
+  private functionName: string;
+  private variableName: string;
+  private x = 1;
+  private isClosed: boolean = false;
+
+  private most_recent_target_door_state;
+
+  private requestTimeout = 7000;
+
+  constructor(
+    private readonly platform: ParticleHomebridgePlatform,
+    private readonly accessory: PlatformAccessory
+  ) {
+    this.Service = this.platform.Service;
+    this.Characteristic = this.platform.Characteristic;
+    this.TargetDoorState = this.Characteristic.TargetDoorState;
+    this.CurrentDoorState = this.Characteristic.CurrentDoorState;
+
+    this.most_recent_target_door_state = this.TargetDoorState.CLOSED;
+
+    const config = this.accessory.context.device;
+
+    this.accessToken = config['access_token'];
+    this.deviceId = config['device_id'];
+    // this.url = config['url'];
+    this.doorOpensInSeconds = config['doorOpensInSeconds'];
+    this.eventName = config['doorStateChangedEventName'];
+    this.functionName = config['doorOpenCloseFunctionName'];
+    this.variableName = config['doorOpenSensorVariableName'];
+
+    // set accessory information
+    this.accessory
+      .getService(this.Service.AccessoryInformation)!
+      .setCharacteristic(this.Characteristic.Manufacturer, 'Particle.io')
+      .setCharacteristic(this.Characteristic.Model, 'Photon')
+      .setCharacteristic(this.Characteristic.SerialNumber, this.deviceId);
+
+    // get the LightBulb service if it exists, otherwise create a new LightBulb service
+    // you can create multiple services for each accessory
+    this.garage_service =
+      this.accessory.getService(this.Service.GarageDoorOpener) ||
+      this.accessory.addService(this.Service.GarageDoorOpener);
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.garage_service.setCharacteristic(this.Characteristic.Name, accessory.context.device.name);
+
+    this.garage_service
+      .getCharacteristic(this.TargetDoorState)
+      .on(CharacteristicEventTypes.SET, this.handleTargetDoorStateSet.bind(this))
+      .on(CharacteristicEventTypes.GET, this.handleTargetDoorStateGet.bind(this));
+
+    this.garage_service
+      .getCharacteristic(this.CurrentDoorState)
+      .on(CharacteristicEventTypes.GET, this.handleCurrentDoorStateGet.bind(this));
+    // .onGet(this.handleCurrentDoorStateGet.bind(this));
+
+    var eventUrl = this.url + 'events/' + this.eventName;
+    const particleAuthHeader = { headers: { Authorization: `Bearer ${this.accessToken}` } };
+    const es = new eventSource(eventUrl, particleAuthHeader);
+
+    this.platform.log.debug('registering event: ' + eventUrl);
+
+    es.onerror = function () {
+      console.error('ERROR!');
+    };
+
+    es.addEventListener(this.eventName, this.doorStateDidChange.bind(this), false);
+
+    // this.fetchCurrentState(this.updateStates.bind(this));
+    // setInterval(this.doThings.bind(this), 10000);
+  }
+
+  private doorStateDidChange(e) {
+    var data = JSON.parse(e.data);
+
+    this.platform.log.debug('doorStateDidChange: ' + data.data);
+
+    if (data.data == 'door-opened') {
+      this.updateStates(null, this.CurrentDoorState.OPEN);
+    } else if (data.data == 'door-closed') {
+      this.updateStates(null, this.CurrentDoorState.CLOSED);
+    }
+  }
+
+  private fetchCurrentState(callback?: Function): any {
+    const isClosedUrl =
+      this.url + 'devices/' + this.deviceId + '/' + this.variableName + '?access_token=' + this.accessToken;
+
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), this.requestTimeout);
+    fetch(isClosedUrl, { signal: controller.signal })
+      .then((response) => {
+        clearTimeout(id);
+        // this.platform.log.debug('response', response);
+        // indicates whether the response is successful (status code 200-299) or not
+        if (!response.ok) {
+          throw new Error(`Request failed with status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const result = data['result'];
+        if (result == 0) {
+          // 0 from Particle means closed
+          if (callback) callback(null, this.CurrentDoorState.CLOSED);
+          else return this.CurrentDoorState.CLOSED;
+        } else if (result == 1) {
+          if (callback) callback(null, this.CurrentDoorState.OPEN);
+          else return this.CurrentDoorState.OPEN;
+        } else {
+          this.platform.log.error(`Cannot get ${this.variableName}`);
+          if (callback) callback(new Error(`Cannot get ${this.variableName}`), null);
+          else return new Error(`Cannot get ${this.variableName}`);
+        }
+      })
+      .catch((error) => {
+        this.platform.log.error(error);
+        // if (error instanceof Error && error.name === 'AbortError') {
+        //   this.platform.log.error(`Timeout while retrieving ${this.variableName}`);
+        //   callback(new Error(`Timeout while retrieving ${this.variableName}`), null);
+        // }
+        if (callback) callback(error, null);
+        else return error;
+      });
+  }
+
+  private updateStates(error, state) {
+    this.platform.log.debug('Updating states: isClosed=', state == this.CurrentDoorState.CLOSED);
+    if (!error) {
+      this.isClosed = state == this.CurrentDoorState.CLOSED;
+      this.most_recent_target_door_state = this.isClosed
+        ? this.TargetDoorState.CLOSED
+        : this.TargetDoorState.OPEN;
+
+      this.garage_service.updateCharacteristic(this.TargetDoorState, this.most_recent_target_door_state);
+      this.garage_service.updateCharacteristic(this.CurrentDoorState, state);
+    } else {
+      // this.garage_service.updateCharacteristic(this.TargetDoorState, error);
+      this.garage_service.updateCharacteristic(this.CurrentDoorState, error);
+    }
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   */
+  private handleTargetDoorStateSet(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.platform.log.debug('handleTargetDoorStateSet() ->', value);
+    this.most_recent_target_door_state = value;
+
+    const newCurrentState =
+      value == this.TargetDoorState.OPEN ? this.CurrentDoorState.OPENING : this.CurrentDoorState.CLOSING;
+    this.garage_service.updateCharacteristic(this.CurrentDoorState, newCurrentState);
+
+    const openCloseUrl = this.url + 'devices/' + this.deviceId + '/' + this.functionName;
+
+    const params = new URLSearchParams();
+    params.append('access_token', this.accessToken);
+    params.append('args', value == this.TargetDoorState.OPEN ? 'open' : 'close');
+
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), this.requestTimeout);
+
+    fetch(openCloseUrl, {
+      method: 'POST',
+      body: params,
+      signal: controller.signal,
+    })
+      .then((response) => {
+        clearTimeout(id);
+        if (response.ok) {
+          callback();
+        } else {
+          this.throwNotRespondingError();
+        }
+      })
+      .catch((error) => {
+        this.platform.log.error(error);
+        callback(error);
+      });
+
+    setTimeout(
+      this.fetchCurrentState.bind(this, this.updateStates.bind(this)),
+      (this.doorOpensInSeconds || 20) * 1000
+    );
+  }
+
+  private handleTargetDoorStateGet(callback: CharacteristicGetCallback) {
+    this.platform.log.debug('handleTargetDoorStateGet() ->', this.most_recent_target_door_state);
+    callback(null, this.most_recent_target_door_state);
+  }
+
+  private handleCurrentDoorStateGet(callback: CharacteristicGetCallback) {
+    this.platform.log.debug('handleCurrentDoorStateGet() -> ');
+    this.fetchCurrentState(this.updateStates.bind(this));
+    callback(HAPStatus.SUCCESS, null);
+  }
+
+  // private handleCurrentDoorStateGet(): Promise<CharacteristicValue> {
+  //   this.platform.log.debug('handleCurrentDoorStateGet() -> ');
+
+  //   return new Promise((resv, rejt) => {
+  //     const state = this.fetchCurrentState();
+  //     if (state instanceof Error) {
+  //       rejt(state);
+  //     } else {
+  //       resv(state);
+  //     }
+  //   });
+  // callback(HAPStatus.SUCCESS, null);
+  // }
+
+  private throwNotRespondingError() {
+    throw new this.platform.api.hap.HapStatusError(
+      this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+    );
+  }
+}
+
+// doThings() {
+//   // start closing the door
+//   if (this.x == 1) {
+//     this.x = 2;
+//     this.garage_service.updateCharacteristic(this.TargetDoorState, this.TargetDoorState.CLOSED);
+//     this.garage_service.updateCharacteristic(this.CurrentDoorState, this.CurrentDoorState.CLOSING);
+//   } else if (this.x == 2) {
+//     this.x = 3;
+//     //door is fully closed (sends notification)
+//     this.garage_service.updateCharacteristic(this.CurrentDoorState, this.CurrentDoorState.CLOSED);
+//   } else if (this.x == 3) {
+//     this.x = 4;
+//     // starting opening the door (sends notification)
+//     this.garage_service.updateCharacteristic(this.TargetDoorState, this.TargetDoorState.OPEN);
+//     this.garage_service.updateCharacteristic(this.CurrentDoorState, this.CurrentDoorState.OPENING);
+//   } else if (this.x == 4) {
+//     this.x = 1;
+//     // door is fully open
+//     this.garage_service.updateCharacteristic(this.CurrentDoorState, this.CurrentDoorState.OPEN);
+//   }
+// }
--- src/platform.ts
+++ src/platform.ts
@@ -1,25 +1,29 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+import {
+  API,
+  DynamicPlatformPlugin,
+  Logger,
+  PlatformAccessory,
+  PlatformConfig,
+  Service,
+  Characteristic,
+} from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { GarageOpenerPlatformAccessory } from './GarageAccessory';
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class ParticleHomebridgePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
-  constructor(
-    public readonly log: Logger,
-    public readonly config: PlatformConfig,
-    public readonly api: API,
-  ) {
+  constructor(public readonly log: Logger, public readonly config: PlatformConfig, public readonly api: API) {
     this.log.debug('Finished initializing platform:', this.config.name);
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
@@ -50,32 +54,19 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * must not be registered again to prevent "duplicate UUID" errors.
    */
   discoverDevices() {
-
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    // First, find all devices in config of type 'garage_door_opener'
+    const garage_doors = this.config['devices'].filter((device) => device.type === 'garage_door_opener');
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-
+    for (const device of garage_doors) {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.device_id + device.doorOpenCloseFunctionName);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+      const existingAccessory = this.accessories.find((accessory) => accessory.UUID === uuid);
 
       if (existingAccessory) {
         // the accessory already exists
@@ -87,7 +78,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        new GarageOpenerPlatformAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
         // remove platform accessories when no longer present
@@ -95,10 +86,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.name);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.name, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
@@ -106,11 +97,20 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        new GarageOpenerPlatformAccessory(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
+
+    // TODO
+    // Next, find all devices in config of type 'walk-in-door'
+    // const walk_in_doors = this.config['devices'].find(device => device.type === 'walk-in-door');
+
+    // // loop over the discovered devices and register each one if it has not already been registered
+    // for (const device of walk_in_doors) {
+
+    // }
   }
 }
